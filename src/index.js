600fffd021c463dc68efd69ca57074b0a3156824
EmmanuelD69 <emmanueldurouge@yahoo.com>
2020-10-13 17:50:13 +0200

    Building / importing / exporting a React Component

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+/* importing App from app.js*/
+import App from "./app";
+
+ReactDOM.render(<App/>, document.querySelector("#root"));
